7d7c8f94c61d953fea0bbd4598646812a1e49e81
dhanashreeg368 <dhanashreeg368@gmail.com>
2023-04-26 14:54:47 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # FileUpload
+
+**Title: File Upload and Short Link Generation Platform**   
+
+*Tech Stack Used: HTML, CSS, JavaScript, Django and MongoDB*
+
+*Features:*   
+1. Front-end:
+○ User interface that allows users to select and upload a file.
+○ Display a short link after the file is successfully uploaded.
+○ Error handling for invalid file types or file size restrictions.
+○ Download link for the uploaded file.     
+
+2. Back-end:
+○ Server-side API that handles file upload and short link generation.
+○ Store the uploaded file on the server or on a cloud storage service (e.g., AWS
+S3).
+○ Generate a unique short link for each uploaded file.
+○ Store the short link and the corresponding file information in a database (e.g.,
+MySQL, MongoDB).
+  
--- assignment/myapp/views.py
+++ assignment/myapp/views.py
@@ -4,20 +4,24 @@ from django.shortcuts import render, redirect
 from pymongo import MongoClient
 from django.shortcuts import render
 
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+# Select the database
+db = client['mydatabase']
+# Select the collection
+col = db['mycollection']
+
 def home_page(request):
+    # return db.list_collection_names()
     return render(request, 'index.html')
 
 def upload_file(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
-        # Connect to MongoDB
-        client = MongoClient('mongodb://localhost:27017/')
-        # Select the database
-        db = client['mydatabase']
-        # Select the collection
-        col = db['mycollection']
+        
         # Insert the file to the database
         file_id = col.insert_one({'filename': myfile.name, 'filedata': myfile.read()}).inserted_id
+        
         # Generate a short link for the file
         short_link = 'http://example.com/files/' + str(file_id)
         return render(request, 'upload_success.html', {'short_link': short_link})
